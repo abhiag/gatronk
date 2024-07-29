26f8daeb7bd704c061290a69c6d1c2d15161189e
abhiag <36300569+abhiag@users.noreply.github.com>
2024-07-29 13:40:17 +0530

    Update gatronk.sh

--- gatronk.sh
+++ gatronk.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
-curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
 sleep 4
 
 sudo apt-get update && sudo apt get upgrade -y
