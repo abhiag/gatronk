b66c3f401ee6f20700dde37eb8dab8badb33e0d2
abhiag <36300569+abhiag@users.noreply.github.com>
2024-07-29 14:00:41 +0530

    Update erc721.sh

--- erc721.sh
+++ erc721.sh
@@ -145,4 +145,4 @@ echo "Minting NFT..."
 npx hardhat run scripts/mint.js --network swisstronik
 echo "NFT minted."
 
-echo "Done! Subscribe: https://t.me/feature_earning"
+echo "Done! Subscribe: https://t.me/GaCryptOfficial"
